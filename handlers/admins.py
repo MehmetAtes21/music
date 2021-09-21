16e9c9c0c664300ed36e4caea3762ee02237c262
EfsaneStar <82456249+Mehmetbaba55@users.noreply.github.com>
2021-09-21 23:26:39 +0300

    Add files via upload

---
+++ handlers/admins.py
@@ -0,0 +1,75 @@
+from asyncio.queues import QueueEmpty
+
+from pyrogram import Client
+from pyrogram.types import Message
+from callsmusic import callsmusic
+
+from config import BOT_NAME as BN
+from helpers.filters import command, other_filters
+from helpers.decorators import errors, authorized_users_only
+
+
+@Client.on_message(command("duraklat") & other_filters)
+@errors
+@authorized_users_only
+async def pause(_, message: Message):
+    if (
+            message.chat.id not in callsmusic.pytgcalls.active_calls
+    ) or (
+            callsmusic.pytgcalls.active_calls[message.chat.id] == 'Duraklatıldı'
+    ):
+        await message.reply_text("❗ Hiçbir şey çalmıyor!")
+    else:
+        callsmusic.pytgcalls.pause_stream(message.chat.id)
+        await message.reply_text("▶️ Duraklatıldı!")
+
+
+@Client.on_message(command("devam") & other_filters)
+@errors
+@authorized_users_only
+async def resume(_, message: Message):
+    if (
+            message.chat.id not in callsmusic.pytgcalls.active_calls
+    ) or (
+            callsmusic.pytgcalls.active_calls[message.chat.id] == 'Oynanıyor'
+    ):
+        await message.reply_text("❗ Hiçbir şey duraklatılmadı!")
+    else:
+        callsmusic.pytgcalls.resume_stream(message.chat.id)
+        await message.reply_text("⏸ Devam etti!")
+
+
+@Client.on_message(command("son") & other_filters)
+@errors
+@authorized_users_only
+async def stop(_, message: Message):
+    if message.chat.id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("❗ Hiçbir şey yayınlanmıyor!")
+    else:
+        try:
+            callsmusic.queues.clear(message.chat.id)
+        except QueueEmpty:
+            pass
+
+        callsmusic.pytgcalls.leave_group_call(message.chat.id)
+        await message.reply_text("❌ Akış durduruldu!")
+
+
+@Client.on_message(command("atla") & other_filters)
+@errors
+@authorized_users_only
+async def skip(_, message: Message):
+    if message.chat.id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("❗ Atlanacak müzik yok!")
+    else:
+        callsmusic.queues.task_done(message.chat.id)
+
+        if callsmusic.queues.is_empty(message.chat.id):
+            callsmusic.pytgcalls.leave_group_call(message.chat.id)
+        else:
+            callsmusic.pytgcalls.change_stream(
+                message.chat.id,
+                callsmusic.queues.get(message.chat.id)["file"]
+            )
+
+        await message.reply_text("➡️ Sıradaki şarkıya atlandı!")
