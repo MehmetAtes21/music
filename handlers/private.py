eb388ec746128a81885d33418d25c6d5022b9047
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-10-23 21:04:12 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-from config import BOT_NAME as bn
+from config import BOT_NAME, BOT_NAME as bn
 from helpers.filters import other_filters2
 
 
