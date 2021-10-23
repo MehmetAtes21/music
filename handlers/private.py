e86541739619bb9401a258215c2b673fcbbd0b5e
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-10-23 21:08:33 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-from config import BOT_NAME, BOT_NAME as bn
+from config import BOT_USARNAME, BOT_NAME as bot
 from helpers.filters import other_filters2
 
 
-@Client.on_message(other_filters2)
+@Client.on_message(command(["start", f"start@{BOT_USARNAME}"]))
 async def start(_, message: Message):
     await message.reply_photo("https://i.ibb.co/khRz42f/Turkish-Voice.jpg")
     await message.reply_text(
@@ -41,7 +41,7 @@ Sesli sohbetlerde müzik çalabilen botum. Ban yetkisiz, Ses yönetimi yetkisi v
      disable_web_page_preview=True
     )
 
-@Client.on_message(filters.command("reload") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command(["reload", f"reload@{BOT_USARNAME}"]) & ~filters.private & ~filters.channel)
 async def reload(_, message: Message):
       await message.reply_text("""**Yeniden başlatıldı. Bot çalışıyor ✅**""",
       reply_markup=InlineKeyboardMarkup(
