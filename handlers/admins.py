c429c5a8cb8c9834f8aacffd850fe2fd64f30877
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-10-31 16:21:18 +0300

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -121,20 +121,3 @@ async def change_ses(client, message):
        await message.reply(f"✅ **Birim olarak ayarlandı:** ```{range}%```")
     except Exception as e:
        await message.reply(f"**hata:** {e}")
-
-
-# Sohbet grubunuzda bilgi yazarak bilgi alınız. Diger botların komutu (help) sorun olmasın diye (bilgi) yapıldı. 
-
-@Client.on_message(command(["bilgi"]) & other_filters) 
-async def bilgi(client, message:Message):
-      await message.reply_text(f"**Merhaba {message.from_user.mention}!\n Bu botun bilgi menüsü 🤩\n\n ▶️ /play - şarkı çalmak için youtube url'sine veya şarkı dosyasına yanıt verme\n ▶️ /play <song name> - istediğiniz şarkıyı çalın\n 🔴 /ytplay <Sorgu> - youtube üzerinden istediğiniz şarkıyı çalar.\n 🎵 /bul <song name> - istediğiniz şarkıları hızlı bir şekilde bulun\n 🎵 /vbul istediğiniz videoları hızlı bir şekilde bulun\n 🔍 /ara <query> - youtube'da ayrıntıları içeren videoları arama\n\n Yalnızca yöneticiler için..\n ▶️ /resume - şarkı çalmaya devam et\n ⏹ /end - müzik çalmayı durdurma\n 🔼 /ver botun sadece yönetici için kullanılabilir olan komutlarını kullanabilmesi için kullanıcıya yetki ver\n 🔽 /al botun yönetici komutlarını kullanabilen kullanıcının yetkisini al\n 🎚 /ses asistan hesabın ses seviyesini kontrol et\n\n ⚪ /katil - Müzik asistanı grubunuza katılır\n ⚫ /ayril - Müzik asistanı grubunuzu terk eder.**", 
-      reply_markup=InlineKeyboardMarkup(
-             [
-                 [
-                     InlineKeyboardButton(
-                         "⚙ Geliştirici", url="https://t.me/Sohbetdestek")
-                 ]
-             ]
-         )
-    )
-
