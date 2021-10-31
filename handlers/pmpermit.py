81f6474db4e9a20f8cc8e68d3da515a4d5081e0f
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-10-31 15:10:40 +0300

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -6,5 +6,4 @@ from pyrogram.types import Chat, Message, User
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
   await USER.send_message(message.chat.id,"Merhaba, Müzik asistanı hizmetidir.\n\n ❗️ kurallar:\n - Sohbete izin yok.\n - Bilgi ve Komutlarım için grup sohbetine /bilgi yazarsanız. komutlarımı ögrenebilirsiniz. \n - İstenmeyen postaya izin verilmez \n\n 🚨 **USERBOT GRUBUNUZA KATILAMAZSA GRUP DAVETI BAĞLANTISI VEYA KULLANICI ADI GÖNDER.**\n\n ⚠️ DİKKAT: Burada bir mesaj gönderiyorsanız Yöneticinin iletinizi göreceği anlamına gelir ve sohbete katılın\n - Bu kullanıcıyı gizli gruplara eklemeyin.\n   - Özel bilgileri burada paylaşmayınız. 📚 Bilgi için @Mahoaga\n\n")
-  return
-                      
+  return                      
