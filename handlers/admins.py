389df2f3e40b202909ba848bbf84bc631bff3172
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-10-31 15:20:25 +0300

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -75,8 +75,12 @@ async def skip(_, message: Message):
         await message.reply_text("⏭️ **__Şarkı bir sonraki kuyruğa atlatıldı__**")
 
 
-@Client.on_message(command("bilgi") & other_filters) 
-async def bilgi(_, message: Message):
+@Client.on_message(
+    filters.command("bilgi")
+    & filters.group
+    & ~ filters.edited
+)
+async def helper(client , message:Message):
       await message.reply_text(f"**Merhaba {message.from_user.mention}!\n Bu botun bilgi menüsü 🤩\n\n ▶️ /play - şarkı çalmak için youtube url'sine veya şarkı dosyasına yanıt verme\n ▶️ /play <song name> - istediğiniz şarkıyı çalın\n 🔴 /ytplay <Sorgu> - youtube üzerinden istediğiniz şarkıyı çalar.\n 🎵 /bul <song name> - istediğiniz şarkıları hızlı bir şekilde bulun\n 🎵 /vbul istediğiniz videoları hızlı bir şekilde bulun\n 🔍 /ara <query> - youtube'da ayrıntıları içeren videoları arama\n\n Yalnızca yöneticiler için..\n ▶️ /resume - şarkı çalmaya devam et\n ⏹ /end - müzik çalmayı durdurma\n 🔼 /ver botun sadece yönetici için kullanılabilir olan komutlarını kullanabilmesi için kullanıcıya yetki ver\n 🔽 /al botun yönetici komutlarını kullanabilen kullanıcının yetkisini al\n 🎚 /ses asistan hesabın ses seviyesini kontrol et\n\n ⚪ /katil - Müzik asistanı grubunuza katılır\n ⚫ /ayril - Müzik asistanı grubunuzu terk eder.**", 
       reply_markup=InlineKeyboardMarkup(
              [
