981cee5a11a1b84bd9b2a3a80dc990852199e81a
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-11-01 13:02:58 +0300

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -20,10 +20,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("Aramanın bağımsız değişkene ihtiyacı var!")
+            await message.reply_text("Bana Sanatçı ismi ya da Şarkı ismi verin!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("Arıyorum....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
