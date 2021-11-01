7e9287d3a482e1e5ee11c6d218b71c51cd646904
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-11-01 12:57:33 +0300

    Update ytp.py

--- handlers/ytp.py
+++ handlers/ytp.py
@@ -18,7 +18,7 @@ from helpers.errors import DurationLimitError
 from helpers.gets import get_url, get_file_name
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-@Client.on_message(command("["ytplay", "dinle"]") & other_filters)
+@Client.on_message(command(["ytplay", "dinle"]) & other_filters)
 @errors
 async def ytplay(_, message: Message):
 
