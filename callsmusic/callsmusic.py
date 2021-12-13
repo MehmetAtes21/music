38800ffe18fa387772afc4758ebe31a43949cc50
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-12-13 19:14:58 +0300

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -1,23 +1,30 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
+from pytgcalls.types import Update
+from pytgcalls.types.input_stream import InputAudioStream, InputStream
 
-import config
-from . import queues
+from config import API_HASH, API_ID, SESSION_NAME
+from callsmusic import queues
 
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
 
 
 @pytgcalls.on_stream_end()
-def on_stream_end(chat_id: int) -> None:
+async def on_stream_end(client: PyTgCalls, update: Update) -> None:
+    chat_id = update.chat_id
     queues.task_done(chat_id)
 
     if queues.is_empty(chat_id):
-        pytgcalls.leave_group_call(chat_id)
+        await pytgcalls.leave_group_call(chat_id)
     else:
-        pytgcalls.change_stream(
-            chat_id, queues.get(chat_id)["file"]
+        await pytgcalls.change_stream(
+            InputStream(
+                InputAudioStream(
+                    queues.get(chat_id)["file"],
+                ),
+            ),
         )
 
 
-run = pytgcalls.run
+run = pytgcalls.start
