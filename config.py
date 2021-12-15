b490c8e386c29c00c61a6e6324a8b71e3d6efd4d
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-12-15 10:46:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME", "TaliaMüzik") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/0f6f8a8a5ad69fe5ecf3d.png")
 BOT_USERNAME = getenv("BOT_USERNAME", "Efsanestar_bot")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
