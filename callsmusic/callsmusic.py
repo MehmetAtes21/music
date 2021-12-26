b54d2038e298c6d741feb0abf38cfb93d2d6625d
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-12-26 17:11:35 +0300

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -1,105 +1,32 @@
-from typing import Dict
-
-from pytgcalls import GroupCallFactory
-
-from callsmusic import client
-from queues import queues
-
-instances: Dict[int, GroupCallFactory] = {}
-active_chats: Dict[int, Dict[str, bool]] = {}
+from . import queues
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+from pytgcalls.types import Update
+from pytgcalls.types.input_stream import InputAudioStream
+from pytgcalls.types.input_stream import InputStream
+from config import API_HASH, API_ID, SESSION_NAME
 
 
-def init_instance(chat_id: int):
-    if chat_id not in instances:
-        instances[chat_id] = GroupCallFactory(
-            client, outgoing_audio_bitrate_kbit=320
-        ).get_file_group_call()
-
-    instance = instances[chat_id]
-
-    @instance.on_playout_ended
-    async def ___(__, _):
-        queues.task_done(chat_id)
-
-        if queues.is_empty(chat_id):
-            await stop(chat_id)
-        else:
-            instance.input_filename = queues.get(chat_id)["file"]
+client = Client(SESSION_NAME, API_ID, API_HASH)
+pytgcalls = PyTgCalls(client)
 
 
-def remove(chat_id: int):
-    if chat_id in instances:
-        del instances[chat_id]
+@pytgcalls.on_stream_end()
+async def on_stream_end(client: PyTgCalls, update: Update) -> None:
+    chat_id = update.chat_id
+    queues.task_done(chat_id)
 
-    if not queues.is_empty(chat_id):
-        queues.clear(chat_id)
-
-    if chat_id in active_chats:
-        del active_chats[chat_id]
-
-
-def get_instance(chat_id: int) -> GroupCallFactory:
-    init_instance(chat_id)
-    return instances[chat_id]
-
-
-async def start(chat_id: int):
-    await get_instance(chat_id).start(chat_id)
-    active_chats[chat_id] = {"playing": True, "muted": False}
+    if queues.is_empty(chat_id):
+        await pytgcalls.leave_group_call(chat_id)
+    else:
+        await pytgcalls.change_stream(
+            chat_id, 
+            InputStream(
+                InputAudioStream(
+                    queues.get(chat_id)["file"],
+                ),
+            ),
+        )
 
 
-async def stop(chat_id: int):
-    await get_instance(chat_id).stop()
-
-    if chat_id in active_chats:
-        del active_chats[chat_id]
-
-
-async def set_stream(chat_id: int, file: str):
-    if chat_id not in active_chats:
-        await start(chat_id)
-    get_instance(chat_id).input_filename = file
-
-
-def pause(chat_id: int) -> bool:
-    if chat_id not in active_chats:
-        return False
-    elif not active_chats[chat_id]["playing"]:
-        return False
-
-    get_instance(chat_id).pause_playout()
-    active_chats[chat_id]["playing"] = False
-    return True
-
-
-def resume(chat_id: int) -> bool:
-    if chat_id not in active_chats:
-        return False
-    elif active_chats[chat_id]["playing"]:
-        return False
-
-    get_instance(chat_id).resume_playout()
-    active_chats[chat_id]["playing"] = True
-    return True
-
-
-async def mute(chat_id: int) -> int:
-    if chat_id not in active_chats:
-        return 2
-    elif active_chats[chat_id]["muted"]:
-        return 1
-
-    await get_instance(chat_id).set_is_mute(True)
-    active_chats[chat_id]["muted"] = True
-    return 0
-
-
-async def unmute(chat_id: int) -> int:
-    if chat_id not in active_chats:
-        return 2
-    elif not active_chats[chat_id]["muted"]:
-        return 1
-
-    await get_instance(chat_id).set_is_mute(False)
-    active_chats[chat_id]["muted"] = False
-    return 0
+run = pytgcalls.start
