a2db5550d111e4072cd344a523238c666fbecdf7
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-12-27 17:50:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import requests
-from pytgcalls import idle
-from callsmusic import run
-from handlers import __version__
 from pyrogram import Client as Bot
-from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 
-
-response = requests.get(BG_IMAGE)
-with open("./etc/foreground.png", "wb") as file:
-    file.write(response.content)
+from callsmusic import run
+from config import API_ID, API_HASH, BOT_TOKEN
 
 
 bot = Bot(
@@ -16,11 +9,8 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="handlers"),
+    plugins=dict(root="handlers")
 )
-
-print(f"[INFO]: VEEZ MUSIC v{__version__} STARTED !")
 
 bot.start()
 run()
-idle()
