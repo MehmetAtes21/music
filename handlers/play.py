140659f9aa0753f6600e99a14940048a7beaa0d7
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2022-02-18 14:30:01 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -1,6 +1,7 @@
 import os
 from os import path
 from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery
 from pyrogram.types import Message, Voice, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserAlreadyParticipant
 from callsmusic import callsmusic, queues
