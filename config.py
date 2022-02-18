b1c9d0e62a4fd1f7f5a980c6c007749dc1369896
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2022-02-18 15:22:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 from os import getenv
-
 from dotenv import load_dotenv
 
 load_dotenv()
+que = {}
+admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
@@ -10,7 +11,7 @@ BOT_NAME = getenv("BOT_NAME", "TaliaMüzik")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME", "Efsanestar_bot")
-
+PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
