d068cf50d030308d1716fae3fd394e15f4df75a4
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2022-02-18 15:50:40 +0300

    Update converter.py

--- converter/converter.py
+++ converter/converter.py
@@ -1,9 +1,9 @@
-from os import path
 import asyncio
+from os import path
 
-from helpers.errors import FFmpegReturnCodeError
-
-
+class FFmpegReturnCodeError(Exception):
+    pass
+  
 async def convert(file_path: str) -> str:
     out = path.basename(file_path)
     out = out.split(".")
@@ -15,15 +15,27 @@ async def convert(file_path: str) -> str:
     if path.isfile(out):
         return out
 
-    proc = await asyncio.create_subprocess_shell(
-        f"ffmpeg -y -i {file_path} -f s16le -ac 1 -ar 48000 -acodec pcm_s16le {out}",
-        asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
-    )
+    try:
+        proc = await asyncio.create_subprocess_shell(
+            cmd=(
+                "ffmpeg " 
+                "-y -i " 
+                f"{file_path} "
+                "-f s16le "
+                "-ac 1 "
+                "-ar 48000 "
+                "-acodec pcm_s16le " 
+                f"{out}"
+            ),
+            stdin=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
 
-    await proc.communicate()
+        await proc.communicate()
 
-    if proc.returncode != 0:
+        if proc.returncode != 0:
+            raise FFmpegReturnCodeError("FFmpeg did not return 0")
+
+        return out
+    except:
         raise FFmpegReturnCodeError("FFmpeg did not return 0")
-
-    return out
