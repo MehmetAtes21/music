d1b67770e138c4da8c3e43fb415eb2558fdd83db
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2022-04-28 00:39:18 +0300

    Create showid.py

---
+++ handlers/showid.py
@@ -0,0 +1,49 @@
+from pyrogram import Client, filters
+from config import BOT_NAME 
+from helpers.get_file_id import get_file_id
+from helpers.filters import command
+
+
+@Client.on_message(command(["id"]))
+async def showid(client, message):
+    chat_type = message.chat.type
+
+    if chat_type == "private":
+        user_id = message.chat.id
+        await message.reply_text(
+            f"<code>{user_id}</code>",
+            quote=True
+        )
+
+    elif chat_type in ["group", "supergroup"]:
+        _id = ""
+        _id += (
+            "<b>Grubun Sohbet kimliği</b>: "
+            f"<code>{message.chat.id}</code>\n"
+        )
+        if message.reply_to_message:
+            _id += (
+                "<b>Kullanıcının Kimlik bilgisi</b>: "
+                f"<code>{message.reply_to_message.from_user.id}</code>\n"
+            )
+            file_info = get_file_id(message.reply_to_message)
+            if file_info:
+                _id += (
+                    f"<b>{file_info.message_type}</b>: "
+                    f"<code>{file_info.file_id}</code>\n"
+                )
+        else:
+            _id += (
+                "<b>Kullanıcı Kimliği</b>: "
+                f"<code>{message.from_user.id}</code>\n"
+            )
+            file_info = get_file_id(message)
+            if file_info:
+                _id += (
+                    f"<b>{file_info.message_type}</b>: "
+                    f"<code>{file_info.file_id}</code>\n"
+                )
+        await message.reply_text(
+            _id,
+            quote=True
+        )
