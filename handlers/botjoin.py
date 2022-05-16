55284adb6a935b4129eed18aa4d8894605108917
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2022-05-16 04:18:40 +0300

    Update botjoin.py

--- handlers/botjoin.py
+++ handlers/botjoin.py
@@ -33,7 +33,7 @@ async def addchannel(client, message):
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b>🔵 Zaman Aşımı Hatası 🔵\n User {user.first_name} userbot için yoğun katılma istekleri nedeniyle grubunuza katılamadı! Asistanın grupta yasaklanmadığından emin olun."
+            f"<b>🔵 Hata 🔵\n User {user.first_name} userbot için yoğun katılma istekleri nedeniyle grubunuza katılamadı! Asistanın grupta yasaklanmadığından emin olun."
             "\n\n Yada Asistan Hesabını Gruba Kendin Ekle </b>",
         )
         return
