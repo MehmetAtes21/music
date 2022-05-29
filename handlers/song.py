a9b1d6111cf372a083aad5d1e76a8bb6eb368f48
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2022-05-29 11:35:00 +0300

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -1,4 +1,4 @@
-# Telegramda yani ben boş işler müdürü :) <> @Mahoaga Tarafından düzenlenen ufak çaplı proje. 
+# Telegramda yani ben boş işler müdürü :) <> @Hayiboo Tarafından düzenlenen ufak çaplı proje. 
 import os
 import requests
 import aiohttp
@@ -55,7 +55,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"▶️ **Şarkı**: [{title[:35]}]({link})\n🔖 **Kaynak**: **YouTube**\n⏳ **Süre**: `{duration}`\n🔗 **Görünümler**: `{views}`\n✍🏻 **Tarafından**: @StarMuzikBot"
+        rep = f"▶️ **Şarkı**: [{title[:35]}]({link})\n⏳ **Süre**: `{duration}`\n•> [𝖲𝗍𝖺𝗋 𝖬𝗎𝗓𝗂𝗄 𝖡𝗈𝗍](https://t.me/StarMuzikBot) 𝖳𝖺𝗋𝖺𝖿𝗂𝗇𝖽𝖺𝗇 !"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
