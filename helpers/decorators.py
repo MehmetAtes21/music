ebfdd784e47a18798071e30b8381a7dc6b7370bf
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2022-05-29 05:06:39 +0300

    Create decorators.py

---
+++ helpers/decorators.py
@@ -0,0 +1,45 @@
+from typing import Callable
+
+from pyrogram import Client
+from pyrogram.types import Message
+
+from helpers.admins import get_administrators
+from config import SUDO_USERS
+
+
+def errors(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        try:
+            return await func(client, message)
+        except Exception as e:
+            await message.reply(f"{type(e).__name__}: {e}")
+
+    return decorator
+
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+        administrators = await get_administrators(message.chat)
+
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                return await func(client, message)
+
+    return decorator
+
+
+# Utils Helper
+def humanbytes(size):
+    """baytları Baytlara Dönüştür ki İnsan Okuyabilsin"""
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
