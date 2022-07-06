a830cbd344b99b17748c09cb39fb39da70d25add
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2022-07-06 15:26:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "TaliaMüzik") 
+BOT_NAME = getenv("BOT_NAME", "StarMuzik") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "Efsanestar_bot")
+BOT_USERNAME = getenv("BOT_USERNAME", "StarMuzikBot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
