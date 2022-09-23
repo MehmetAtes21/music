eba6c9d0f5eb469eccc0a7258676b6af563e94a6
Ramo <112237128+ramoben200@users.noreply.github.com>
2022-09-24 02:33:57 +0300

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -18,7 +18,7 @@ async def pmPermit(client: USER, message: Message):
                 return
             await USER.send_message(
                 message.chat.id,
-                "**Merhaba, Müzik asistanı hizmetidir**.\n [🧑🏻‍💻 Developer](https://t.me/Ballasresmi)\n",
+                "**Merhaba, Müzik asistanı hizmetidir**.\n [🧑🏻‍💻 Developer](https://t.me/BOT_RAMO)\n",
             )
             return
  
