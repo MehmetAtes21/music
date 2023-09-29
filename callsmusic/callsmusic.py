8868ba6899e9e4bdd3a859aff19c3add749fe75e
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2023-09-29 16:15:19 +0300

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -4,25 +4,29 @@ from pytgcalls.types import Update
 from pytgcalls.types.input_stream import InputStream
 from pytgcalls.types.input_stream import InputAudioStream
 from config import API_HASH, API_ID, SESSION_NAME
-from pyrogram import filters
 
 from . import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
 
+
 @pytgcalls.on_stream_end()
-async def on_stream_end(context, is_connected):
-    chat_id = -1001983841726 - context.full_chat.id
-    if is_connected:
-        await app.send_message(chat_id, 'Successfully joined!')
+async def on_stream_end(client: PyTgCalls, update: Update) -> None:
+    chat_id = update.chat_id
+    queues.task_done(chat_id)
+
+    if queues.is_empty(chat_id):
+        await pytgcalls.leave_group_call(chat_id)
     else:
-        await app.send_message(chat_id, 'Disconnected from voice chat..')
-
-
-@Client.on_message(filters.outgoing & filters.command('join'))
-async def join_handler(_, message):
-    await group_call.start(message.chat.id)
+        await pytgcalls.change_stream(
+            chat_id, 
+            InputStream(
+                InputAudioStream(
+                    queues.get(chat_id)["file"],
+                ),
+            ),
+        )
 
 
 run = pytgcalls.start
