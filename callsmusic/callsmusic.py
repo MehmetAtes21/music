a2bfa782d09c12a98b20749b70d4ba791b0f22fc
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2023-09-29 16:10:14 +0300

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -4,6 +4,7 @@ from pytgcalls.types import Update
 from pytgcalls.types.input_stream import InputStream
 from pytgcalls.types.input_stream import InputAudioStream
 from config import API_HASH, API_ID, SESSION_NAME
+from pyrogram import Client
 
 from . import queues
 
@@ -19,7 +20,7 @@ async def on_stream_end(context, is_connected):
         await app.send_message(chat_id, 'Disconnected from voice chat..')
 
 
-@app.on_message(filters.outgoing & filters.command('join'))
+@Client.on_message(filters.outgoing & filters.command('join'))
 async def join_handler(_, message):
     await group_call.start(message.chat.id)
 
